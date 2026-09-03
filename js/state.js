3bc488357d7ca857bd0822423b598742df0ae17d
amlakdot <xixlogin@gmail.com>
2026-09-04 00:05:05 +0330

    Create state.js

---
+++ js/state.js
@@ -0,0 +1,14 @@
+/* =========================================================
+   STATE
+========================================================= */
+
+export const state = {
+  token: null,
+  files: [],
+  currentFilter: "all",
+  search: "",
+  editingFileId: null,
+  isSaving: false,
+  pollTimer: null,
+  lastSyncSha: null
+};
