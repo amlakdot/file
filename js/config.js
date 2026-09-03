99e0a05a544b43b8d2a29b4589d73bc7b0c86fb3
amlakdot <xixlogin@gmail.com>
2026-09-04 00:04:32 +0330

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,12 @@
+/* =========================================================
+   CONFIG
+========================================================= */
+
+export const CONFIG = {
+  owner: "amlakdot",
+  repo: "file",
+  branch: "main",
+  dataPath: "data/files.json",
+  githubApi: "https://api.github.com",
+  pollInterval: 5 * 60 * 1000
+};
